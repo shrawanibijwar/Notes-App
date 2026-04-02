2c7af05360b20e38f637f1c9be7bdc38a3c75346
Shrawani Bijwar <shrawanibijwar@gmail.com>
2026-04-02 13:31:38 +0530

    updated port for deployment

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ app.delete('/delete/:filename', function(req, res) {
     });
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,function(){
-    console.log("Its running");
+app.listen(PORT, function(){
+    console.log(`Server running on port ${PORT}`);
 });
